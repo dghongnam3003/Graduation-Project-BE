1d52c28b9b40976ef76a9867d4f38262c781708c
dghongnam3003 <dghongnam@gmail.com>
2025-06-09 12:29:11 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  apps: [{
+    name: 'pumpfund-backend',
+    script: 'dist/src/main.js',
+    instances: 1,
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3004
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+}
